cf287498c28b45a8316cf8d66e6ce31f580749f1
kristellu <kristell.urueta@gmail.com>
2020-04-09 02:48:06 -0500

    Forms LogIn

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'SPOTIFLY';
+  public title = 'SPOTIFLY';
+  public user: User;
+  public indentity; //Comprobar datos del usuario 
+  public token;
+
+  constructor(){
+    this.user = new User('','','','','','ROLE_USER','');
+
+  }
+
+  public onSubmit(){
+    console.log(this.user);
+  }
 }
